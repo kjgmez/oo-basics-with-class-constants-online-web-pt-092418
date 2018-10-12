3dd4e4f1178ddb0dfe3d0cd658b81625ea8c9a12
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-12 02:51:57 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,11 +6,6 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.push(brand)
-  end
-
-  def brand=(brand)
-    @brand = brand
     if brand == "Nike" || brand == "Uggs"
       BRANDS.push(brand)
     end
